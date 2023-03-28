1b9949a5e9283f7a50c3663ed4fb0db91303d28e
Marcus10110 <markgarrison@saleae.com>
2023-03-28 10:39:13 -0700

    first commit

---
+++ Program.cs
@@ -0,0 +1,16 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Net.Client;
+
+namespace Logic2App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            using var channel = GrpcChannel.ForAddress("http://127.0.0.1:10430");
+            var client = new Saleae.Automation.Manager.ManagerClient(channel);
+            var reply = client.GetAppInfo(new Saleae.Automation.GetAppInfoRequest { });
+            Console.WriteLine("Logic 2 App Version: " + reply.AppInfo.ApplicationVersion);
+        }
+    }
+}
---
+++ readme.md
@@ -0,0 +1,19 @@
+# Logic2-automation C# dotnet core example
+
+Based on example from https://swaminathanvetri.in/2020/01/29/getting-started-with-grpc-dotnet-in-vs-code/
+
+Requires dotnet core SDK to be installed, version 7.0.102 was used to generate this project.
+
+This example requires the Logic 2 automation server to be running, on default port 10430.
+
+Usage:
+
+```
+dotnet run
+```
+
+Expected output:
+
+```
+Logic 2 App Version: 2.4.7
+```
